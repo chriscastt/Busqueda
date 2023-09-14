31c2a61f70e053d1d11a048fb0ba01f412d8dcec
chriscastt <144874575+chriscastt@users.noreply.github.com>
2023-09-13 22:55:33 -0600

    Add files via upload

---
+++ Busqueda.py
@@ -0,0 +1,17 @@
+list = [1, 3, 5, 7, 9, 11, 13]
+
+
+num_s = int(input("Ingrese un número: "))
+
+# Utilizar el operador "in" para verificar si el número está en la lista
+if num_s in list:
+    print(f"El número {num_s} está presente en la lista.")
+else:
+    print(f"El número {num_s} no está presente en la lista.")
+
+
+
+
+
+
+
